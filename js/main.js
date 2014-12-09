c6e9756ce08ccb7dfb8ad99b5a44675048063a1f
andres-root <andres.root.coder@gmail.com>
2014-12-08 21:08:18 -0500

    working on xhr

--- js/main.js
+++ js/main.js
@@ -19,8 +19,20 @@ var xhrGet = function(url, successHandler, errorHandler) {
   xhr.send();
 };
 
+    
+
 xhrGet('js/gallery.json', function(data) {
-	console.log(data);
+	var images = JSON.parse(data);
+	for(var i = 0; i < images.legth; i++) {
+	  	$( ".carousel-inner" ).append( '<div class="item active">
+					      <img src="img/image_1.jpg" alt="">
+					      <div class="carousel-caption">
+					        <h3>Experimental Projects at Intel</h3>
+					        <p>June 17, 2013</p>
+					      </div>
+					    </div>' );
+	 }
+
 }, function(status) {
 	alert('Something went wrong.');
 });
