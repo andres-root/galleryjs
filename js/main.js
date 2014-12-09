8a1f087b2b06be3b49f0632c8c0d8f298e3139c6
andres-root <andres.root.coder@gmail.com>
2014-12-08 21:23:54 -0500

    working on xhr

--- js/main.js
+++ js/main.js
@@ -1,30 +1,32 @@
-var xhrGet = function(url, successHandler, errorHandler) {
- 	var xhr = typeof XMLHttpRequest != 'undefined'
-    	? new XMLHttpRequest()
-    	: new ActiveXObject('Microsoft.XMLHTTP');
-  	xhr.open('get', url, true);
-  	xhr.onreadystatechange = function() {
-    	var status;
-    	var data;
-    	if (xhr.readyState == 4) {
-	      	status = xhr.status;
-	      	if (status == 200) {
-		        data = JSON.parse(xhr.responseText);
-	    	    successHandler && successHandler(data);
-	      	} else {
-	        	errorHandler && errorHandler(status);
-	      	}
-    	}
-  };
-  xhr.send();
-};
+$(document).ready(function() {
+	var xhrGet = function(url, successHandler, errorHandler) {
+	 	var xhr = typeof XMLHttpRequest != 'undefined'
+	    	? new XMLHttpRequest()
+	    	: new ActiveXObject('Microsoft.XMLHTTP');
+	  	xhr.open('get', url, true);
+	  	xhr.onreadystatechange = function() {
+	    	var status;
+	    	var data;
+	    	if (xhr.readyState == 4) {
+		      	status = xhr.status;
+		      	if (status == 200) {
+			        data = JSON.parse(xhr.responseText);
+		    	    successHandler && successHandler(data);
+		      	} else {
+		        	errorHandler && errorHandler(status);
+		      	}
+	    	}
+	  };
+	  xhr.send();
+	};
 
-xhrGet('js/gallery.json', function(data) {
-	console.log(data)
-	for (var i = 0; i < data.length; i++) {
-		image = data[i]
-		$(".carousel-inner").append($('<div class="item"><img src="' + image.image + '"><div class="carousel-caption"><h3>' + image.title + '</h3><p>' + image.date + '</p></div></div>'));
-	}
-}, function(status) {
-	alert('Something went wrong.');
+	xhrGet('js/gallery.json', function(data) {
+		console.log(data)
+		for (var i = 0; i < data.length; i++) {
+			image = data[i]
+			$(".carousel-inner").append($('<div class="item"><img src="' + image.image + '"><div class="carousel-caption"><h3>' + image.title + '</h3><p>' + image.date + '</p></div></div>'));
+		}
+	}, function(status) {
+		alert('Something went wrong.');
+	});
 });
