a0d37b054c19f720fed12c2c6c201a727e2a847c
andres-root <andres.root.coder@gmail.com>
2014-12-08 19:02:52 -0500

    working on xhr

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
-function xhrGet(reqUri, type, callback) {
+function xhrGet(reqUri, callback) {
 	var xhr = new XMLHttpRequest();
 	xhr.open("GET", reqUri, true);
 	xhr.onload = callback;
 	xhr.send();
 }
+
+xhrGet('gallery_json.js', function (data) {
+    var response = JSON.parse(data.responseText);
+    console.log(response);
+});
