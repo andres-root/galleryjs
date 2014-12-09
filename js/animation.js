6681abb66cfe4aaa7e7b268cb6100da3964b4836
andres-root <andres.root.coder@gmail.com>
2014-12-08 23:31:38 -0500

    update

--- js/animation.js
+++ js/animation.js
@@ -5,7 +5,12 @@ var context = canvas.getContext("2d");
 var font_size = 10;
 var columns = canvas.width;
 var codes = [];
-var matrixCode = "ANDRESFELIPELUJANRODRIGUEZ";
+var matrixCode = "I saw the best minds of my generation destroyed by madness,
+     starving hysterical naked,
+dragging themselves through the negro streets at dawn looking 
+     for an angry fix,
+angelheaded hipsters burning for the ancient heavenly
+     connection to the starry dynamo in the machinery of nightANDRESFELIPELUJANRODRIGUEZ";
 
 
 matrixCode = matrixCode.split("");
