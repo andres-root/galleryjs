439649269042404382532f63e471f0c894ef5ddc
andres-root <andres.root.coder@gmail.com>
2014-12-08 23:05:34 -0500

    update

--- js/controllers.js
+++
@@ -1,7 +0,0 @@
-var galleryApp = angular.module('galleryApp', []);
-
-galleryApp.controller('ImageListCtrl', function ($scope, $http) {
-	$http.get('js/gallery.json').success(function(data) {
-	    $scope.images = data;
-	  });
-});
