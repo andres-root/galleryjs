e235b8762e67aeba85561cf3e46363dee6b560d7
andres-root <andres.root.coder@gmail.com>
2014-12-08 22:20:06 -0500

    update

--- js/main.js
+++ js/main.js
@@ -20,33 +20,21 @@ $(document).ready(function() {
 
 	xhrGet('js/gallery.json', function(data) {
 		var inner = $('.carousel-inner');
+		var indicators = $('.carousel-indicators');
 		for(var i = 0; i < data.length; i++) {
-						var slide = $('<div class="item" data-sequence="' + i + '">');
-						
-						if(i == 0) { slide.addClass('active'); }
-						
-						var img = $('<img src="' + data[i].image + '">');
-		 
-						slide.append(img);
-						
-						// create a caption for slide
-						var caption = $('<div class="carousel-caption">');
-						caption.append('<h4>'+data[i].title+'</h4>');
-						caption.append('<p>'+data[i].date+'</p>');
-		 
-						// add the caption to the slide
-						slide.append(caption);
-						
-						// put the slide in the .carousel-inner container
-						inner.append(slide);
-						
-						// add the dot indicator for the slide			
-						var indicator = $('<li data-target="#carousel" data-slide-to="'+i+'">');
-						if(i == 0) { indicator.addClass('active'); }
-						indicators.append(indicator);
-					}	
-		 
-					// boot the bootstrap carousel plugin
+			var slide = $('<div class="item" data-sequence="' + i + '">');
+			if(i == 0) { slide.addClass('active'); }
+			var img = $('<img src="' + data[i].image + '">');
+			slide.append(img);
+			var caption = $('<div class="carousel-caption">');
+			caption.append('<h4>'+data[i].title+'</h4>');
+			caption.append('<p>'+data[i].date+'</p>');
+			slide.append(caption);
+			inner.append(slide);
+			var indicator = $('<li data-target="#carousel" data-slide-to="'+i+'">');
+			if(i == 0) { indicator.addClass('active'); }
+			indicators.append(indicator);
+		}	
 	}, function(status) {
 		alert('Something went wrong.');
 	});
