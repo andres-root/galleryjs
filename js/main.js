6b859235d6ecd0aa77e9bde502c1bb153b26573d
andres-root <andres.root.coder@gmail.com>
2014-12-08 22:09:51 -0500

    update

--- js/main.js
+++ js/main.js
@@ -19,13 +19,56 @@ $(document).ready(function() {
 	};
 
 	xhrGet('js/gallery.json', function(data) {
-		for (var i = 0; i < data.length; i++) {
-			var image = data[i];
-			console.log(image);
-			$(".carousel-inner").append('Hello');
-			//$(".carousel-inner").append('<div class="item"><img src="' + image['image'] + '"><div class="carousel-caption"><h3>' + image['title'] + '</h3><p>' + image['date'] + '</p></div></div>');
-		}
+		for(var i = 0; i < data.length; i++) {
+						// create a slide with the class .item which boostrap carousel will manage
+						var slide = $('<div class="item" data-sequence="' + i + '">');
+						
+						// if it's the first one mark it a active
+						if(i == 0) { slide.addClass('active'); }
+						
+						// create a viewport for centering the image in the container
+						var viewport = $('<div>');
+						
+						// create an lazy loading image
+						var img = $('<img data-lazy-src="' + data[i].src + '">');
+		 
+						// put the image in the viewport
+						viewport.append(img);
+						
+						// put the viewport in the slide
+						slide.append(viewport);
+						
+						// create a caption for slide
+						var caption = $('<div class="carousel-caption">');
+						caption.append('<h4>'+data[i].title+'</h4>');
+						caption.append('<p>'+data[i].date+'</p>');
+		 
+						// add the caption to the slide
+						slide.append(caption);
+						
+						// put the slide in the .carousel-inner container
+						inner.append(slide);
+						
+						// make the viewport a container that is always the same size as the carousel
+						viewport.digitopiaContainer({fillContainer:inner})
+						
+						// make the viewport a digitopiaViewport that can enlarge images if needed
+						viewport.digitopiaViewport({ blowup: true, crop:true, align:'center', listenTo: viewport });
+						
+						// make the image lazy
+						img.digitopiaLazyImg();
+			
+						// add the dot indicator for the slide			
+						var indicator = $('<li data-target="#carousel" data-slide-to="'+i+'">');
+						if(i == 0) { indicator.addClass('active'); }
+						indicators.append(indicator);
+					}	
+		 
+					// boot the bootstrap carousel plugin
+					this.element.carousel({pause:'hover'});
 	}, function(status) {
 		alert('Something went wrong.');
 	});
 });
+
+
